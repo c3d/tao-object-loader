cfbf2dbd717dfb69d5630d128178d52bb102f4b3
Jerome Forissier <jerome@taodyne.com>
2011-08-23 11:44:10 +0200

    Add forms of has_display_mode and enable_stereoscopy that take text arguments. Refs #1315.
    This is useful to support display names such as "2dplusdepth" or "2D-plus-Depth".

--- object_loader/object_loader.cpp
+++ object_loader/object_loader.cpp
@@ -55,10 +55,7 @@ Tree_p object(Tree_p self,
     {
         Object3D *obj = Object3D::Object(name);
         if (!obj)
-        {
-            Ooops("File not found or unreadable: $1", self);
             return XL::xl_false;
-        }
 
         Object3DDrawing *drawing = new Object3DDrawing(obj, x, y, z, w, h, d);
         Object3D::tao->addToLayout(Object3DDrawing::render_callback, drawing,
@@ -86,10 +83,7 @@ Tree_p object(Tree_p self, Text_p name)
     {
         Object3D *obj = Object3D::Object(name);
         if (!obj)
-        {
-            Ooops("File not found or unreadable: $1", self);
             return XL::xl_false;
-        }
 
         Object3D::tao->scheduleRender(Object3D::render_callback, obj);
     }
