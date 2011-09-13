c8fb4c4f3a3d2c3c13ca410d01b674a623d6ab21
Jerome Forissier <jerome@taodyne.com>
2011-09-13 14:57:45 +0200

    Revert "GLC_Lib patch: allow rendering with no material."
    This reverts commit 8b46e76c723c9946e56dbb5a412ac933eccb4c71.

--- glc-lib/geometry/glc_geometry.cpp
+++ glc-lib/geometry/glc_geometry.cpp
@@ -259,9 +259,7 @@ void GLC_Geometry::render(const GLC_RenderProperties& renderProperties)
 	renderWire= renderWire || ((renderProperties.renderingFlag() != glc::TransparentRenderFlag) && !isTransparent());
 	if (!m_IsWire || renderWire)
 	{
-		bool renderNullOverWriteMaterial= (renderProperties.renderingMode() == glc::OverwriteMaterial);
-		renderNullOverWriteMaterial= renderNullOverWriteMaterial && (NULL == renderProperties.overwriteMaterial());
-		if (m_MaterialHash.isEmpty() && !m_IsWire && !renderNullOverWriteMaterial)
+		if (m_MaterialHash.isEmpty() && !m_IsWire)
 		{
 			GLC_Material* pMaterial= new GLC_Material();
 			pMaterial->setName(name());
@@ -271,7 +269,7 @@ void GLC_Geometry::render(const GLC_RenderProperties& renderProperties)
 		m_IsSelected= renderProperties.isSelected();
 
 		// Define Geometry's property
-		if(!GLC_State::isInSelectionMode() && !renderNullOverWriteMaterial)
+		if(!GLC_State::isInSelectionMode())
 		{
 			glPropGeom(renderProperties);
 		}
