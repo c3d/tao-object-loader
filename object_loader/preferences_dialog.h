6300146278106f78f9cb798d241d493d666d53b9
Catherine Burvelle <catherine@taodyne.com>
2011-10-16 17:19:49 +0200

    Merge branch 'master' of ssh://nano.taodyne.com/~ddd/Work/tao

---
+++ object_loader/preferences_dialog.h
@@ -0,0 +1,48 @@
+// ****************************************************************************
+//  preferences_dialog.h                                           Tao project
+// ****************************************************************************
+//
+//   File Description:
+//
+//    Display a message dialog to allow user to configure the module
+//
+//
+//
+//
+//
+//
+//
+//
+// ****************************************************************************
+// This software is property of Taodyne SAS - Confidential
+// Ce logiciel est la propriété de Taodyne SAS - Confidentiel
+//  (C) 2011 Jerome Forissier <jerome@taodyne.com>
+//  (C) 2011 Taodyne SAS
+// ****************************************************************************
+
+#ifndef PREFERENCES_DIALOG_H
+#define PREFERENCES_DIALOG_H
+
+#include <QDialog>
+#include <QString>
+
+// QSettings group name to read/write preferences
+#define OBJLOADER_GROUP "ObjectLoader"
+
+class PreferencesDialog : public QDialog
+{
+    Q_OBJECT
+public:
+    PreferencesDialog(QWidget *parent = 0);
+    ~PreferencesDialog() {}
+
+public slots:
+    void           useVBOToggled(bool use);
+
+public:
+    static bool    useVBOs();
+    static bool    useVBOsDefault();
+};
+
+
+#endif // PREFERENCES_DIALOG_H
