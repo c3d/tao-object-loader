380ea24541275561f99cbde9db0138c2685bdb91
Christophe de Dinechin <christophe@taodyne.com>
2012-01-17 17:31:13 +0100

    Insufficiently tested code in load_text, fixes #1811.

--- object_loader/object3d.cpp
+++ object_loader/object3d.cpp
@@ -183,7 +183,7 @@ void Object3D::DrawObject()
         licensed = tao->checkLicense("ObjectLoader 1.0", false);
         tested = true;
     }
-    if (!licensed && !tao->blink(4.5, 0.5, 300.0))
+    if (!licensed && !tao->blink(4.5, 0.5))
         return;
 
     checkCurrentContext();
